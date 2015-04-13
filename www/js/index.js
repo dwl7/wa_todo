2ae9428be346b1ae9d05ada75b581a170c17c6da
dwl7 <dwl7@sfu.ca>
2015-04-12 18:57:40 -0700

    finished v1.0

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ 
 var app = {
     // Application Constructor
     initialize: function() {
@@ -47,3 +48,4 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
